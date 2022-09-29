2475273fdb9b653b3aaebe53de56d744777c1874
gamzeunal95 <gamzeunal9505@gmail.com>
2022-09-29 15:25:38 +0300

    Revize

--- NorthWindSql/StoredProcedure.sql
+++ NorthWindSql/StoredProcedure.sql
@@ -1,56 +1,59 @@
 ﻿
---CREATE PROCEDURE [dbo].[Toplama]
---	@param1 int = 0,
---	@param2 int
---AS
---Begin --Başlangıç demek
+CREATE PROCEDURE [dbo].[Toplama]
+	@param1 int = 0,
+	@param2 int
+AS
+Begin --Başlangıç demek
 
---	SELECT @param1 +  @param2
+	SELECT @param1 +  @param2
 
---RETURN 0
+RETURN 0
 
---End
+End
 
----- ctrl shift e diyerek kaydet .
----- Kaydettikten sonra çalıştırmak için şu sorguyu at.
---exec dbo.Toplama (5,3)
+-- ctrl shift e diyerek kaydet .
+-- Kaydettikten sonra çalıştırmak için şu sorguyu at.
+exec dbo.Toplama (5,3)
 
----- İki Tarih Arasındaki Siparişleri Görmek için oluturduğumuz hazır procedur.
---CREATE PROCEDURE [dbo].[İkiTarihArasıSiparisler]
---	@BaslangicTarihi DateTime,
---	@BitisTarihi DateTime
---AS
---Begin 
+-- İki Tarih Arasındaki Siparişleri Görmek için oluturduğumuz hazır procedur.
+CREATE PROCEDURE [dbo].[İkiTarihArasıSiparisler]
+	@BaslangicTarihi DateTime,
+	@BitisTarihi DateTime
+AS
+Begin 
 
---	SELECT * From Orders
---	Where OrderDate between @BaslangicTarihi and @BitisTarihi
+	SELECT * From Orders
+	Where OrderDate between @BaslangicTarihi and @BitisTarihi
 
---RETURN 0
+RETURN 0
 
---End
----- Kaydettikten sonra çalıştırmak için şu sorguyu at.
---exec dbo.İkiTarihArasıSiparisler '1996-07-04 00:00:00.000' , '1996-07-31 00:00:00.000'
+End
 
--- Ulkelere gore siparisleri getirmek için bu soruyu yazıp procedure olarak ekledik
---Create procedure dbo.UlkeSiparisleri
---@ulke nvarchar(15)
---As
---Begin
---	Select * from Orders where ShipCountry = @ulke
+-- Kaydettikten sonra çalıştırmak için şu sorguyu at.
+exec dbo.İkiTarihArasıSiparisler '1996-07-04 00:00:00.000' , '1996-07-31 00:00:00.000'
 
---End
+ Ulkelere gore siparisleri getirmek için bu soruyu yazıp procedure olarak ekledik
+Create procedure dbo.UlkeSiparisleri
+@ulke nvarchar(15)
+As
+Begin
+	Select * from Orders where ShipCountry = @ulke
 
-----Kaydettikten sonra çalıştırmak için şu sorguyu at.
---Exec dbo.UlkeSiparisleri 'USA'   --dbo. yazmadan da çalıştırabilirsin.
+End
+
+--Kaydettikten sonra çalıştırmak için şu sorguyu at.
+Exec dbo.UlkeSiparisleri 'USA'   --dbo. yazmadan da çalıştırabilirsin.
+
 
 -- Customer tablosunda şehirin listesini getiren bir procedur yazalım.
---Create procedure dbo.CustomerSehirler
---@sehir nvarchar(20)
---As
---Begin
---	Select * from Customers where City = @sehir
+Create procedure dbo.CustomerSehirler
+@sehir nvarchar(20)
+As
+Begin
+	Select * from Customers where City = @sehir
 
---End
+End
 
-----Sorgudada adını verdiğim şehiri listelesin
---exec dbo.CustomerSehirler 'London'
+--Sorgudada adını verdiğim şehiri listelesin
+exec dbo.CustomerSehirler 'London'
+
